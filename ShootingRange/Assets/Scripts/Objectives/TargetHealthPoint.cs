faee469bc2585df1dbb335cafc43456bb2f93056
Alan Vuong <alanavuong@gmail.com>
2016-08-21 17:29:34 -0500

    Script:Target Health Point Id:4 Type:Main
    This script gives health to an object.
    
    The target is destroyed upon zero or less health. If the target is destroyed, the game keeps track of the number of destroyed targets. This is because the winning condition is about destroying a certain number of targets.

---
+++ ShootingRange/Assets/Scripts/Objectives/TargetHealthPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetHealthPoint : MonoBehaviour {
+
+	//how many health point the target has
+	public float totalHealth;//starting health points for target
+	private float currentHealth;//health decrease upon damage
+
+	void Start()
+	{
+		currentHealth = totalHealth;//so health is not connected with other health script
+	}
+
+	//this check for damage dealt on target
+	void applyDamage(float damage)
+	{
+		currentHealth -= damage;//target lose health
+		if (currentHealth <= 0) {//health below or at zero means object destroys itself
+			GameObject.Find ("Objective Location").SendMessage("targetCounter", 1);//reduce number of target needed to be destroy by one
+			Destroy (gameObject);
+		}
+	}
+}
