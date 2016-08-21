1d5230bd3b28aeddb340f3412fa40d18c25cd6db
Alan Vuong <alanavuong@gmail.com>
2016-08-21 17:37:21 -0500

    Script:Win Conditions Id:5 Type:Main
    This script gives the user interface to the player on game progress.
    
    The user interface shows number of targets and a timer.

---
+++ ShootingRange/Assets/Scripts/Objectives/WinConditions.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/*
+ * This scripts relates to the objective whether player wins/loses
+ * */
+
+public class WinConditions : MonoBehaviour {
+
+	public Text victoryText;//display victory message
+	public Text unfinishedObjectiveText;//display ongoing mission message
+	public Text gameOverText; //display lose when conditions are met
+
+	public RaycastShooting getRaycastScript;
+	//public CountdownTimer getCountdownScript;
+
+
+	public Text timerText;//this text will print out the remaning time
+	private float minutes;//use for a countdown timer
+	private float seconds;
+	public float startingTime;//the starting time for a countdown
+
+	public int requiredTargetDestroyed;//the number of target needed to destroy to win
+
+	private bool timerStop = false;// play or pause timer
+
+	void Start(){
+		victoryText.text = "";//default no message in the beginning
+		gameOverText.text = "";//default no message in the beginning
+		hideUnfinishedObjective ();
+		timerText.text = "";
+		hideUnfinishedObjective ();
+		//currentHealth = maxHealth;
+	}
+
+	void Update () {
+		if (timerStop == false) {
+			getTimer ();//Call the time function
+		}
+
+		if(startingTime < 1){//if timer is out of time than display game over
+			displayGameOver ();
+		}
+	}
+	
+	//check if player reach their desination and goa
+	void OnTriggerStay(Collider player)
+	{
+		//print (getCountdownScript.startingTime);
+		if ((player.tag == "Player") && (requiredTargetDestroyed <= 0) && (startingTime > 1)) {
+			timerStop = true;//timer has stop
+			hideUnfinishedObjective();//turn off if already inside						
+			victoryText.text = "You Win!";
+		} else if (player.tag == "Player" && startingTime < 1) {
+			displayGameOver ();
+			hideUnfinishedObjective();
+		}else if(player.tag == "Player"){
+			unfinishedObjectiveText.text = "You haven't destroyed all the targets yet.";//Targets still remaining
+		}
+	}
+
+	//remove remaining objective if player is not present at objective
+	void OnTriggerExit(Collider player){//tell player if they didn't finish their objective upon reaching the goal
+		hideUnfinishedObjective ();
+	}
+
+	//creates a countdown timer
+	void getTimer()
+	{
+		if (Mathf.Floor (startingTime) != 0) {//timer keep tickin down until it reach zero
+			startingTime = startingTime - (Time.deltaTime);//decreases the time limit by real time
+			
+			minutes = Mathf.Floor (startingTime) / 60;//convert 60 seconds for every one minute
+			seconds = Mathf.Floor (startingTime) % 60;//when less than 60 seconds than it spills over as 
+			//remaining seconds
+			if (seconds >= 10) {
+				timerText.text = "Time Remaining: " + Mathf.Floor (minutes).ToString () + ":" + 
+					seconds.ToString ();
+			} else {//if the remaning seconds is below 10 then it set up a 0 as a placeholder for tens place
+				timerText.text = "Time Remaining: " + Mathf.Floor (minutes).ToString () + ":0" + 
+					seconds.ToString ();
+			}
+		}
+	}
+
+	//if player runs out of time then display game over
+	void displayGameOver()
+	{
+		gameOverText.text = "Game Over";
+	}
+
+	//hide unfinish objective upon leaving the trigger zone
+	void hideUnfinishedObjective(){
+		unfinishedObjectiveText.text = "";
+	}
+
+	//destroy target decrease the number of targets needed to be destroy
+	void targetCounter(int targetDestroy)
+	{
+		requiredTargetDestroyed -= targetDestroy;
+	}
+
+	/*public void damageTarget(float gunDamage){
+		if ((Input.GetKey ("e")) && (currentHealth >= 0)) {
+			currentHealth -= gunDamage;
+		} else {
+			Destroy (target);
+		}*/
+
+}
---
+++ ShootingRange/Assets/Scripts/Player/FirstPersonController.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ *This scripts relate to player movement and controls 
+ */
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;//speed of player
+	private float currentSprintSpeed;
+	public float sprintSpeed;//speed of sprinting player
+	public float mouseSensitivity = 5.0f;//speed of player's camera
+
+	private float horizontalRotation = 0f;
+	private float verticalRotation = 0f;//
+	private float currentAimDownSightSensitivitySpeed;//speed of camera rotation decrease upon aiming down the sights
+	public float aimDownSightSensitivitySpeed;
+	public float rotationVerticalLimit = 60.0f;//how far a player can look up and down
+
+	public GameObject playerCamera;//the first person camera controlled the player
+	//public GameObject player;
+	private CharacterController playerController;
+
+	private float verticalSpeed;//speed going forward and back
+	private float horizontalSpeed;//speed going left and right
+
+	public float playerHeight;//player will transit between height through standing and crouching
+	public float playerCrouchHeight;//player height decrease upon crouching
+	public float crouchingSpeedTransition; //how fast the player moves from standing to crouching or vise versa
+	private float currentCrouchingMovementSpeed;
+	public float crouchingMovementSpeed;//movement speed decrease upon crouching
+	private float playerCurrentHeight;//how player to return to this height
+
+	private bool isCrouching = false;//crooching determines whether sprinting available and speed change
+
+	private float currentAimDownSightMovementSpeed;
+	public float aimDownSightMovementSpeed;//speed when aimming gun
+
+	// Use this for initialization
+	void Start () {
+		//Screen.lockCursor = true;//hide the mouse cursor
+		playerController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rotateCamera ();
+		movePlayer ();
+	}
+
+	//player can control their direction of sight
+	void rotateCamera(){
+		//rotation
+		if (Input.GetKey ("o")) {
+			currentAimDownSightSensitivitySpeed = aimDownSightSensitivitySpeed;
+		} else {
+			currentAimDownSightSensitivitySpeed = 1.0f;
+		}
+		horizontalRotation = Input.GetAxis ("Mouse X") * mouseSensitivity * currentAimDownSightSensitivitySpeed;//keyboard input for horizontal camera movement
+		transform.Rotate (0, horizontalRotation, 0);//move along the horizontal direction with the camera
+		
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity * currentAimDownSightSensitivitySpeed;//keyboard input for vertical movement
+		verticalRotation = Mathf.Clamp (verticalRotation, -rotationVerticalLimit, rotationVerticalLimit);//set limit for vertical direction from max vertical to min vertical direction
+		playerCamera.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);//move along vertical direction for camera 
+		//without interfering with the the horizontal camera
+
+	}
+
+	//relate to movement of player's body
+	void movePlayer(){
+		playerMovementOnXZ_Plane ();
+		playerCrouching ();
+		//playerjump ();
+		/*if (Input.GetButtonDown ("Jump")) {
+			gameObject.transform.position += new Vector3(0, 10, 0);
+		}*/
+
+		/*else{
+			playerController.height = playerHeight;
+		}*/
+
+	}
+
+	//enable forward,backward,left and right movement
+	void playerMovementOnXZ_Plane(){
+
+		//speed increase upon spriting but can't sprint when crouching or aiming down the sights
+		if (Input.GetButton ("Sprint") && isCrouching == false && !Input.GetKey ("o") &&!Input.GetButton ("Move Backward")) {
+			currentSprintSpeed = sprintSpeed;
+		} else {
+			currentSprintSpeed = 1.0f;
+		}
+
+		//speed decrease upon crouching
+		if (isCrouching == true) {
+			currentCrouchingMovementSpeed = crouchingMovementSpeed;
+		} else {
+			currentCrouchingMovementSpeed = 1.0f;
+		}
+
+		//speed decrease upon aiming down the sights
+		if (Input.GetKey ("o")) {
+			currentAimDownSightMovementSpeed = aimDownSightMovementSpeed;
+		} else {
+			currentAimDownSightMovementSpeed = 1.0f;
+		}
+
+		//speed varies upon sprinting, crouching, and aiming down the sights. if the conditions of those three don't apply the speed multiply
+		//won't change current movement speed
+		verticalSpeed = Input.GetAxis ("Vertical") * movementSpeed * currentSprintSpeed * currentCrouchingMovementSpeed * 
+			currentAimDownSightMovementSpeed;
+		horizontalSpeed = Input.GetAxis ("Horizontal") * movementSpeed * currentSprintSpeed * currentCrouchingMovementSpeed * 
+			currentAimDownSightMovementSpeed;
+		/*print ("after");
+		print ("norm" + movementSpeed);
+		print ("fast" + sprintSpeed);
+		print ("ducking" + crouchingMovementSpeed);
+		print ("total" + movementSpeed * sprintSpeed / movementSpeed);*/
+
+		Vector3 speed = new Vector3 (horizontalSpeed, 0, verticalSpeed);//the player moves along the xz plane
+		
+		speed = transform.rotation * speed;//this allow the movement to follow the direction of the camera pointed direction
+		CharacterController playerController = GetComponent<CharacterController> ();//
+		
+		playerController.SimpleMove( speed );
+	}
+
+	//player will move downward with a lower camera view
+	void playerCrouching(){
+		if (Input.GetButton ("Crouch")) {
+			if (playerController.height > playerCrouchHeight) {//keep crouching until minimum height
+				playerController.height -= crouchingSpeedTransition;
+				//playerController.height = playerCrouchHeight;
+			}
+
+		} else if (playerController.height < playerHeight) {//keep standing until maximum height
+			playerController.height += crouchingSpeedTransition;
+		}
+		
+		playerCurrentHeight = playerController.height;//change game controller to match height
+		if (playerCurrentHeight >= playerHeight) {//boolean to determine whether sprinting allowed
+			isCrouching = false;
+		} else {
+			isCrouching = true;
+		}
+	}
+
+	/*void playerjump(){//player jumps into the air once
+		if (Input.GetKey ("j")) {//PLEASE CHANGE THIS INTO A BUTTON
+			print ("You press the jump button");
+			float jumpHeight = 2.0f;
+			float jumpSpeed = 9.8f;
+			Vector3 jumpPosition = new Vector3(horizontalSpeed, jumpHeight + transform.position.y, verticalSpeed);
+
+			Vector3.MoveTowards (transform.position, jumpPosition, jumpSpeed * Time.deltaTime); 
+		}
+	}*/
+}
