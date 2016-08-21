3da165e7a9fdfb1227c075f84303a036eabd0691
Alan Vuong <alanavuong@gmail.com>
2016-08-21 18:39:33 -0500

    Script:Destroy Game Object Boundary Id:7 Type:Main
    This script destroys game objects upon destroying this boundary.
    
    This script is suppose destroy grenades that slip pass the ground boundary.

---
+++ ShootingRange/Assets/Scripts/DestroyGameObjectBoundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyGameObjectBoundary : MonoBehaviour {
+
+	//Destroy grenades that slip through the floor or player
+	void OnTriggerEnter(Collider other)
+	{
+		Destroy (other.gameObject);
+	}
+}
