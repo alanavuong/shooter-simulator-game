c3573eae7f34d0784ccdf2c5fe8e284f5932890d
Alan Vuong <alanavuong@gmail.com>
2016-08-21 19:19:38 -0500

    Script:Ammunition Id:11 Type:Main
    This script turns objects into ammunition.
    
    These objects give ammo for the player's guns.

---
+++ ShootingRange/Assets/Weapons/Guns/Ammunation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ammunation : MonoBehaviour {
+
+	public RaycastShooting getRaycastScript;//access to raycast script
+	public int numberOfClips;//the number of clip picked up
+	private bool excessiveAmmo = false;//boolean determine whether to destroy ammo if at max ammo
+
+	//this script controls quanity of ammo picked up
+
+	//refill some number of clips
+	void OnTriggerStay(Collider other){
+		if (other.tag == "Player") {//detect if player can pick it up automatically
+			excessiveAmmo = getRaycastScript.refillAmmunation (numberOfClips, false);//refill ammo from another script and determine boolean flag
+			if(excessiveAmmo == true){//if player can fit ammo than destroy this item
+				Destroy (gameObject);
+			}
+		}
+	}
+}
