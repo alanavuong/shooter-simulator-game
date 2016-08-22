acab313020ec6a98b56196fef47c4db8a159e28f
Alan Vuong <alanavuong@gmail.com>
2016-08-22 10:20:47 -0500

    Script:Weapon Switching Id:14 Type:Main
    This script switches your weapons.
    
    This allows weapon variety for the player.

---
+++ ShootingRange/Assets/Weapons/Guns/SwitchWeapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchWeapon : MonoBehaviour {
+
+	//this script allows gun switching
+	public bool isHoldingWeapon = false;
+	public RaycastShooting getRaycastScript;
+	public GameObject rifle;
+	public GameObject pistol;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Switch Weapon")) {//clicking button allows switching
+			print ("clicked!");
+			isHoldingWeapon = !isHoldingWeapon;//turn boolean to true/false 
+			rifle.SetActive (!isHoldingWeapon);//one is off than the other is on
+			pistol.SetActive (isHoldingWeapon);
+		}
+
+		getRaycastScript.displayMagazineCount ();//subtract from ammunation count
+		getRaycastScript.displayAmmunationCount ();//display ammo count
+	}
+}
