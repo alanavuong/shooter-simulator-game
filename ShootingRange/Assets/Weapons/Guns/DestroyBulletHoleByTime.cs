c2da4d9ae4c9d6645d73ed815db82b1a0330964a
Alan Vuong <alanavuong@gmail.com>
2016-08-22 18:06:13 -0500

    Script:Destroy Bullet By Time Id:16 Type:Main
    This script destroys a bullet object at a certain time.
    
    This prevents several bullets from staying in the level.

---
+++ ShootingRange/Assets/Weapons/Guns/DestroyBulletHoleByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBulletHoleByTime : MonoBehaviour {
+
+	public float bulletHoleLifespan;
+
+	//destroy bullet hole upon a period of time when spawn
+	void Start () {
+		Destroy (gameObject, bulletHoleLifespan);
+	}
+}
