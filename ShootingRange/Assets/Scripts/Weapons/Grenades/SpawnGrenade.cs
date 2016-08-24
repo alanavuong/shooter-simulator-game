6c1bf06fb2ca7e561c4ef80fad472abea80d2903
Alan Vuong <alanavuong@gmail.com>
2016-08-24 11:27:06 -0500

    Scripts:Spawn Grenade Id:32 Type:Style
    Curly braces were reformated in the whole script.

--- ShootingRange/Assets/Scripts/Weapons/Grenades/SpawnGrenade.cs
+++ ShootingRange/Assets/Scripts/Weapons/Grenades/SpawnGrenade.cs
@@ -2,11 +2,15 @@
 using System.Collections;
 
 //this script will spawn a grenade in front of the player screen
-public class SpawnGrenade : MonoBehaviour {	
+
+public class SpawnGrenade : MonoBehaviour 
+{	
 	public GameObject grenade;
 
-	void Update () {
-		if (Input.GetButtonDown ("Grenade")) {
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Grenade")) 
+		{
 			Instantiate (grenade, transform.position, Quaternion.identity);
 		}
 	}
