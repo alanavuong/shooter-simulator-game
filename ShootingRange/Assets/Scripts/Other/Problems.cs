32ab60ab05415983e17bd9970b0ce1b6ca65e904
Alan Vuong <alanavuong@gmail.com>
2016-08-24 12:25:14 -0500

    Scripts:Throw Grenade Restyle Id:33 Type:Style
    Curly braces were reformated, and comments are rewritten.
    
    The forward velocity is the horizontal and vertical velocity.

--- ShootingRange/Assets/Scripts/Other/Problems.cs
+++ ShootingRange/Assets/Scripts/Other/Problems.cs
@@ -1,65 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//this just a place to list all my to do list for this program
-
 public class Problems : MonoBehaviour {
+	//WHEN FINISHING GAME
+	//make cursor invisible
+	//make code more readiable
+	//fix the text positions for ammo count, win conditions, etc
+	//return fire button to Input.GetButton("Fire1") in RaycastShooting.cs in Gun folders in Weapons folders
+	//create aim down sights button in place for Input.GetKey("o") in RaycastShooting.cs in GUn folders in Weapons folders
+	//don't forget to put back throwing grenade option
 
-	//CURRENT TASKS FOR THIS GAME OR ISSUES
-	//fix progamming style if it is not good enough
-	//give gun a reload time
-	//when making a menu turn cursor on then off when leaving the menu
-	//grenade currently has no count or recovering the ammo count
-	//player is not picking up ammo anymore
-	//set player height in FirstPersonController.cs script to the same height as inspector
-	//change the capsule height upon crouching
-	//fix character controller height or camera height? whatever is better
-	//standing up from crouching seems glitchly and not smooth
-	//player loses camera, gun, and everything upon dropping into the destory boundary
-
-	//SUGGESTION TO SPEED UP THE GAME IF NECCESSARY
+	//WHEN GAME IS LAGGING/SLOWER/INEFFICENT
 	//consider using break upon checking inventory
 	//consider using direct variables instead of SendMessage
 
-	//BEFORE RELEASING THE GAME TO DO LIST
-	//make cursor invisible
-	//fix the text positions for user interface like ammo count, win conditions, etc
-	//change fire button to Input.GetButton("Fire1") in RaycastShooting.cs in Gun folders in Weapons folders
-	//create aim down sights button for Input.GetKey("o") in RaycastShooting.cs in the gun folders in the weapons folders
-	//consider a way to cancel grenade throw
+	//IN PROCRESS
+	//fix documentation style if not good enough
+	//give gun a reload time
+	//when making a menu turn cursor on then off when off the menu
+	//grenade currently has no what you call it ammo count or recovering the ammo count
+	//not picking up ammo anymore
+	//set player height in FirstPersonController.cs script to the same height as inspector
+	//change the capsule height upon crouching
+	//fix character controller height or camera height? whatever is better
+	//set up grenade ammo acount
+	//crouching up seems glitching and not smooth
+	//fix the speed problem and make cleaner code and more readiablet
+	//player lose camera and gun and everything about destruction by boundary below
 
-	//FIXED ISSUES
-	//player can't fire during aiming down the sights 
-
-	//fix the target points count
+	//FIXED
+	//player can't fire during aiming down the sights SOLUTION JUST CHECK IF THERE NOT PRESSING THE AIM DOWN SIGHT BUTTON
+	//fix the target points coun
 	//lower the sensitivity upon zooming in
-
-	//destroy grenade when it falls through the ground 
-	//possible solution:place create a giant boundary destroyer below the ground
-
-	//grenade sometimes don't turn increase trigger size
-
-	//remove the gunSpawnPoint end point
-
+	//Don't allow the player to shoot during shooting motion
+	//destroy grenade over time period if in midair too long POSSIBLE SOLUTION PLACE AT THE A GIANT BOUNDARY DESTROYER
+	//grenade sometimes don't turn into triggers
+	//remove the gunSpawnPoint end point, unessecary
 	//move the spawn grenade point somewhere not on the camera
-
-	//set up a health system to destroy the gameobjects 
-	//solution:use sendmessage may need to change to direct variable
-
-	//decrease speed during aim down sights 	
-	//solution:just check whether they are using the button and modify camera speed
-
-	//fixed grenade fuse time solution use wait for seconds function coroutine and change put it in the write place
-
-	//if player is running and aim down sight. then, aim down sight overriddes the aim down sight motion solution check if they are using aim down sight button
-
-	//it requires two click to fire your gun
-
-	//the player can't fire and aim down the sights	
-	//solution:my keyboard mouse can't do it but a keyboard mouse can do it
-
-	//allow the player to shoot while crouching 
-
-	//player can't sprint and shoot at the same time 
-	//solution:create an if condition that accounts for the sprinting and shooting condition
+	//set up a health system to destroy the gameobjects SOLUTION USE SENDMESSAGE MAY NEED TO CHANGE TO DIRECT VARIABLE
+	//Decrease speed during aim down sights 	SOLUTION:JUST CHECK WHETHER THEY ARE USING THE BUTTON
+	//fixed grenade fuse time SOLUTION USE WAIT FOR SECONDS FUNITON COURTINE(MISPELL) AND CHANGE CODE ORDER
+	//if player is running and aim down sight then aim down sight overriddes the aim down sight motion SOLUTION CHECK IF AIM DOWN SIGHT BUTTON
+	//it requires two click to fire your gun, understand the reason for this condition. problem is probably...
+	//...Input.GetButton("Some key")) SOLUYION ASUS MOUSE X PROBLEM
+	//Consider allow the player to shoot while crouching SOLUTION SET INNER LOOP WITH RAYCAST IF CONDITOON
+	//player can't sprint and shoot at the same time SOLUTION CHECK WHETHER IF PLAYER IS PRESSING THE SPRINT BUTTON
 }
