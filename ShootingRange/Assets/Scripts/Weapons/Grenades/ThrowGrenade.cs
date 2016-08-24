c47464fa5eaec771e858ce893b025f0f86584b7c
Alan Vuong <alanavuong@gmail.com>
2016-08-24 12:46:54 -0500

    Scripts:Throw Grenade Restyle Again Id:37 Type:Style
    Curly braces were reformated, and comments are rewritten.
    
    The forward velocity is explained as a horizontal and vertical velocity.

--- ShootingRange/Assets/Scripts/Weapons/Grenades/ThrowGrenade.cs
+++ ShootingRange/Assets/Scripts/Weapons/Grenades/ThrowGrenade.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 //this script will enable throwing a grenade and exploding after contact with collider after some seconds
-//notes:the grenades is affected by direction the player is pointing with their crosshair
-//notes:the trigger zone exists to allow during game testing to see the radius of the explosion
-public class ThrowGrenade : MonoBehaviour {
+
+public class ThrowGrenade : MonoBehaviour 
+{
 
 	public Rigidbody grenadeRigidbody;
 	public CapsuleCollider playerGrenade;//the collider needs to hit another collider to trigger fuse time
@@ -12,20 +12,19 @@ public class ThrowGrenade : MonoBehaviour {
 	public GameObject player;
 	public GameObject playerCamera;
 	public GameObject objectiveLocation;
-	public float grenadeDamage;//power/damage of grenade upon exploding
-	public float grenadeDistance;//determine how far the grenades travel from point A to point B
-	public float explosionRadius;//grenade explosive radius upon exploding
+	public float grenadeDamage;//damage it deals towards targets
+	public float grenadeDistance;//there really isn't a unit for this
+	public float explosionRadius;//grenade radius on the trigger of an explosion
 	public float waitTime;//fuse time before grenade explosions upon hitting a collider such as a floor
 
-	//the following represents components of a vector to give the grenade the right speed and direction
 	private float verticalDistance;//the direction forward/back on xz-plane
 	private float horizontalDistance;//the direction left/right on xz-plane
-	private float upwardVelocity;//grenade speed going up
-	private float forwardVelocity;//grenade speed thrown forward on the xz-plane including a vertical and 
-								  //horizontal distance component
+	private float upwardVelocity;//grenade speed going upward on the y-axis
+	private float forwardVelocity;//forward velocity is a combination of the vertical and horizontal distance
 
 	//the grenade's velocity
-	void Start(){
+	void Start()
+	{
 		playerGrenade = GetComponent<CapsuleCollider> ();
 		player = GameObject.Find("Player");
 		playerCamera = GameObject.Find ("First Person Camera");
@@ -33,32 +32,42 @@ public class ThrowGrenade : MonoBehaviour {
 
 		upwardVelocity = grenadeDistance * Mathf.Sin (playerCamera.transform.eulerAngles.x * (Mathf.PI / 180));
 		forwardVelocity = grenadeDistance * Mathf.Cos (playerCamera.transform.eulerAngles.x * (Mathf.PI / 180));
+
+		//breaks the down the forward velocity down to a velocity in the x and z coordinate
 		verticalDistance = forwardVelocity * Mathf.Cos (player.transform.eulerAngles.y * (Mathf.PI / 180));
 		horizontalDistance = forwardVelocity * Mathf.Sin (player.transform.eulerAngles.y * (Mathf.PI / 180));
-		//grenade's final velocity is always the same but its velocity component change depending on the cos/sin
-		grenadeRigidbody.velocity = new Vector3 (horizontalDistance, -upwardVelocity, verticalDistance);
+
+		///velocity must take a x and z argument hence breaking upwardVelocity
+		grenadeRigidbody.velocity = new Vector3 (horizontalDistance, -upwardVelocity, verticalDistance); //final grenade velocity
 	}
 
-	//gravity push down 9.8 meters per second
-	void FixedUpdate(){
+	//gravity falls down 9.8 meters per second
+	void FixedUpdate()
+	{
 		grenadeRigidbody.velocity -= new Vector3 (0f, 9.8f * Time.deltaTime, 0f);
 	}
 
-	//if greande hit a gameobject then create explosion within trigger zone
-	IEnumerator OnCollisionEnter(Collision other){
-		if (other.gameObject.tag == "Ground") {//check for the label tag before the fuse time
+	//if the grenades hit the ground then create explosion within trigger zone
+	IEnumerator OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Ground") 
+		{
 			yield return StartCoroutine ("fuseTimer");
 		}
 	}
 
-	//grenade elimates anything of the label target
-	void OnTriggerEnter(Collider other){
-		if (other.gameObject.tag == "Target") {
-			if(other.gameObject != null){
-				GameObject.FindGameObjectWithTag("Target").SendMessage ("applyDamage", grenadeDamage);
+	//grenade damage anything of the label targets
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Target") 
+		{
+			if(other.gameObject != null)
+			{
+				GameObject.FindGameObjectWithTag("Target").SendMessage ("applyDamage", grenadeDamage);//subtract target's health by damage
 			}
 		}
-		Destroy (GameObject.Find ("Grenade(Clone)"));//avoids grenades destroying other grenades
+
+		Destroy (GameObject.Find ("Grenade(Clone)"));//prevents grenade from stopping another grenade from exploding
 	}
 
 	//a fuse time when a grenade hits the ground before it explodes
