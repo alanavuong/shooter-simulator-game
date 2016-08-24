863efaa220cfac4bd9884948e18fb2dfee6374dd
Alan Vuong <alanavuong@gmail.com>
2016-08-24 09:42:10 -0500

    Scripts:Win Conditions Restyle Id:27 Type:Style
    Comments has been rewritten and I have change the spacing.
    
    I remove some redundant comments and improve comments with OnTriggerStay.

--- ShootingRange/Assets/Scripts/Objectives/WinConditions.cs
+++ ShootingRange/Assets/Scripts/Objectives/WinConditions.cs
@@ -2,82 +2,92 @@
 using UnityEngine.UI;
 using System.Collections;
 
-/*
- * This scripts relates to the objective whether player wins/loses
- * */
+//this scripts relates to the objective whether the player wins/loses
 
 public class WinConditions : MonoBehaviour {
 
-	public Text victoryText;//display victory message
 	public Text unfinishedObjectiveText;//display ongoing mission message
+	public Text victoryText;//display victory message on fullfilling certain conidionts
 	public Text gameOverText; //display lose when conditions are met
 
-	public RaycastShooting getRaycastScript;
-	//public CountdownTimer getCountdownScript;
-
-
 	public Text timerText;//this text will print out the remaning time
+	public float startingTime;//the starting time for a countdown
 	private float minutes;//use for a countdown timer
 	private float seconds;
-	public float startingTime;//the starting time for a countdown
+	private bool timerStop = false;//play or pause timer
 
 	public int requiredTargetDestroyed;//the number of target needed to destroy to win
 
-	private bool timerStop = false;// play or pause timer
 
-	void Start(){
-		victoryText.text = "";//default no message in the beginning
-		gameOverText.text = "";//default no message in the beginning
+	void Start()
+	{
+		victoryText.text = "";//you don't want to tell they win in the beginning
+		gameOverText.text = "";//you don't want to tell they lose in the beginning
+		timerText.text = "";//this is just placeholder
 		hideUnfinishedObjective ();
-		timerText.text = "";
-		hideUnfinishedObjective ();
-		//currentHealth = maxHealth;
 	}
 
-	void Update () {
-		if (timerStop == false) {
-			getTimer ();//Call the time function
+	void Update () 
+	{
+		if (timerStop == false) 
+		{
+			getTimer ();
 		}
 
-		if(startingTime < 1){//if timer is out of time than display game over
+		//if timer is out of time than display game over
+		if(startingTime < 1)
+		{
 			displayGameOver ();
 		}
 	}
 	
-	//check if player reach their desination and goa
+	//check if player reach their desination and goal
 	void OnTriggerStay(Collider player)
 	{
-		//print (getCountdownScript.startingTime);
-		if ((player.tag == "Player") && (requiredTargetDestroyed <= 0) && (startingTime > 1)) {
-			timerStop = true;//timer has stop
-			hideUnfinishedObjective();//turn off if already inside						
+		//the player wins the game only if they destroy all targets before time runs out and reach the desination
+		if ((player.tag == "Player") && (requiredTargetDestroyed <= 0) && (startingTime > 1)) 
+		{
+			timerStop = true;
+			hideUnfinishedObjective();//stop showing the current objective if they already won						
 			victoryText.text = "You Win!";
-		} else if (player.tag == "Player" && startingTime < 1) {
+		} 
+		else if (player.tag == "Player" && startingTime < 1) 
+		{
 			displayGameOver ();
 			hideUnfinishedObjective();
-		}else if(player.tag == "Player"){
-			unfinishedObjectiveText.text = "You haven't destroyed all the targets yet.";//Targets still remaining
+		}
+		//if the objective isn't finish execute this
+		else if(player.tag == "Player")
+		{
+			unfinishedObjectiveText.text = "You haven't destroyed all the targets yet.";
 		}
 	}
 
-	//remove remaining objective if player is not present at objective
-	void OnTriggerExit(Collider player){//tell player if they didn't finish their objective upon reaching the goal
+	//if they leave the objective location than stop telling them about the objective isn't finish
+	void OnTriggerExit(Collider player)
+	{
 		hideUnfinishedObjective ();
 	}
 
 	//creates a countdown timer
 	void getTimer()
 	{
-		if (Mathf.Floor (startingTime) != 0) {//timer keep tickin down until it reach zero
+		if (Mathf.Floor (startingTime) != 0) //floor exists to prevent negative seconds
+		{
 			startingTime = startingTime - (Time.deltaTime);//decreases the time limit by real time
 			
 			minutes = Mathf.Floor (startingTime) / 60;//convert 60 seconds for every one minute
 			seconds = Mathf.Floor (startingTime) % 60;//when less than 60 seconds than it spills over as 
-			//remaining seconds
-			if (seconds >= 10) {
+
+			//this just format to show minutes and seconds
+			if (seconds >= 10) 
+			{
 				timerText.text = "Time Remaining: " + Mathf.Floor (minutes).ToString () + ":" + 
 					seconds.ToString ();
-			} else {//if the remaning seconds is below 10 then it set up a 0 as a placeholder for tens place
+			}
+			//if the remaning seconds is below 10 then it set up a 0 as a placeholder for tens place
+			else 
+			{
 				timerText.text = "Time Remaining: " + Mathf.Floor (minutes).ToString () + ":0" + 
 					seconds.ToString ();
 			}
@@ -91,7 +101,8 @@ public class WinConditions : MonoBehaviour {
 	}
 
 	//hide unfinish objective upon leaving the trigger zone
-	void hideUnfinishedObjective(){
+	void hideUnfinishedObjective()
+	{
 		unfinishedObjectiveText.text = "";
 	}
 
@@ -100,12 +111,5 @@ public class WinConditions : MonoBehaviour {
 	{
 		requiredTargetDestroyed -= targetDestroy;
 	}
-
-	/*public void damageTarget(float gunDamage){
-		if ((Input.GetKey ("e")) && (currentHealth >= 0)) {
-			currentHealth -= gunDamage;
-		} else {
-			Destroy (target);
-		}*/
 
 }
