703bd44f5c29714438f931bd97a8e2caf0339746
Alan Vuong <alanavuong@gmail.com>
2016-08-24 12:49:28 -0500

    Scripts:Ammunition Restyle Again Id:38 Type:Style
    Curly braces were reformated, and comments are rewritten.
    
    This is the last script that needed to copy again.

--- ShootingRange/Assets/Scripts/Weapons/Guns/Ammunition.cs
+++ ShootingRange/Assets/Scripts/Weapons/Guns/Ammunition.cs
@@ -1,21 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ammunition : MonoBehaviour {
+//this script controls quanity of ammo picked up
 
-	//public RaycastShooting getRaycastScript;//access to raycast script
+public class Ammunition : MonoBehaviour 
+{
+
+	public RaycastShooting getRaycastScript;//access to raycast script
 	public int numberOfClips;//the number of clip picked up
 	private bool excessiveAmmo = false;//boolean determine whether to destroy ammo if at max ammo
 
-	//this script controls quanity of ammo picked up
-
-	//refill some number of clips
-	void OnTriggerStay(Collider other){
-		if (other.tag == "Player") {//detect if player can pick it up automatically
-			//excessiveAmmo = getRaycastScript.refillAmmunition (numberOfClips, false);//refill ammo from another script and determine boolean flag
-			if(excessiveAmmo == true){//if player can fit ammo than destroy this item
+	//refill upon touching ammunition box
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			excessiveAmmo = getRaycastScript.refillAmmunition (numberOfClips, false);//refill ammo from another script and determine boolean flag
+			//if player has ammo space than destroy this object
+			if(excessiveAmmo == true)
+			{
 				Destroy (gameObject);
 			}
 		}
 	}
+
 }
