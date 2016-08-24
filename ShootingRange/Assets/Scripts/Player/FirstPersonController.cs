f0d0c08bfe0d6ed5365d8f4d8ab1e96c07139769
Alan Vuong <alanavuong@gmail.com>
2016-08-24 11:21:35 -0500

    Scripts:First Person Controller Restyle Id:31 Type:Style
    The comments are more clearer and made format more consisent.
    
    Curly braces were reformated in the whole script.

--- ShootingRange/Assets/Scripts/Player/FirstPersonController.cs
+++ ShootingRange/Assets/Scripts/Player/FirstPersonController.cs
@@ -1,25 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/*
- *This scripts relate to the player's movement and controls
- */
+//this scripts relate to the player's movement and camera
 
 public class FirstPersonController : MonoBehaviour {
 
-	public float movementSpeed = 5.0f;//speed of player
 	private float currentSprintSpeed;
+	public float movementSpeed = 5.0f;//speed of player
 	public float sprintSpeed;//speed of sprinting player
 	public float mouseSensitivity = 5.0f;//speed of player's camera
 
 	private float horizontalRotation = 0f;
-	private float verticalRotation = 0f;//
+	private float verticalRotation = 0f;
 	private float currentAimDownSightSensitivitySpeed;//speed of camera rotation decrease upon aiming down the sights
 	public float aimDownSightSensitivitySpeed;
 	public float rotationVerticalLimit = 60.0f;//how far a player can look up and down
 
 	public GameObject playerCamera;//the first person camera controlled the player
-	//public GameObject player;
 	private CharacterController playerController;
 
 	private float verticalSpeed;//speed going forward and back
@@ -30,16 +27,16 @@ public class FirstPersonController : MonoBehaviour {
 	public float crouchingSpeedTransition; //how fast the player moves from standing to crouching or vise versa
 	private float currentCrouchingMovementSpeed;
 	public float crouchingMovementSpeed;//movement speed decrease upon crouching
-	private float playerCurrentHeight;//how player to return to this height
+	private float playerCurrentHeight;//player's return to these height 
 
 	private bool isCrouching = false;//crooching determines whether sprinting available and speed change
 
-	private float currentAimDownSightMovementSpeed;
-	public float aimDownSightMovementSpeed;//speed when aimming gun
+	private float currentAimDownSightMovementSpeed;//the fixed aim down speed
+	public float aimDownSightMovementSpeed;//aim down sight speed varies based on their hotkeys
 
 	// Use this for initialization
-	void Start () {
-		//Screen.lockCursor = true;//hide the mouse cursor
+	void Start () 
+	{
 		playerController = GetComponent<CharacterController> ();
 	}
 	
@@ -51,73 +48,72 @@ public class FirstPersonController : MonoBehaviour {
 	}
 
 	//player can control their direction of sight
-	void rotateCamera(){
-		//rotation
-		if (Input.GetKey ("o")) {
+	void rotateCamera()
+	{
+		//aim down sight speed slows rotation speed upon clicking this hotkey
+		if (Input.GetKey ("o")) 
+		{
 			currentAimDownSightSensitivitySpeed = aimDownSightSensitivitySpeed;
-		} else {
+		} 
+		else 
+		{
 			currentAimDownSightSensitivitySpeed = 1.0f;
 		}
+
 		horizontalRotation = Input.GetAxis ("Mouse X") * mouseSensitivity * currentAimDownSightSensitivitySpeed;//keyboard input for horizontal camera movement
-		transform.Rotate (0, horizontalRotation, 0);//move along the horizontal direction with the camera
-		
+		transform.Rotate (0, horizontalRotation, 0);//move along the horizontal direction with the camera	
+
 		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity * currentAimDownSightSensitivitySpeed;//keyboard input for vertical movement
 		verticalRotation = Mathf.Clamp (verticalRotation, -rotationVerticalLimit, rotationVerticalLimit);//set limit for vertical direction from max vertical to min vertical direction
 		playerCamera.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);//move along vertical direction for camera 
-		//without interfering with the the horizontal camera
 
 	}
 
 	//relate to movement of player's body
-	void movePlayer(){
+	void movePlayer()
+	{
 		playerMovementOnXZ_Plane ();
 		playerCrouching ();
-		//playerjump ();
-		/*if (Input.GetButtonDown ("Jump")) {
-			gameObject.transform.position += new Vector3(0, 10, 0);
-		}*/
-
-		/*else{
-			playerController.height = playerHeight;
-		}*/
-
 	}
 
 	//enable forward,backward,left and right movement
-	void playerMovementOnXZ_Plane(){
-
+	void playerMovementOnXZ_Plane()
+	{
 		//speed increase upon spriting but can't sprint when crouching or aiming down the sights
-		if (Input.GetButton ("Sprint") && isCrouching == false && !Input.GetKey ("o") &&!Input.GetButton ("Move Backward")) {
+		if (Input.GetButton ("Sprint") && isCrouching == false && !Input.GetKey ("o") &&!Input.GetButton ("Move Backward")) 
+		{
 			currentSprintSpeed = sprintSpeed;
-		} else {
+		} 
+		else 
+		{
 			currentSprintSpeed = 1.0f;
 		}
 
 		//speed decrease upon crouching
-		if (isCrouching == true) {
+		if (isCrouching == true) 
+		{
 			currentCrouchingMovementSpeed = crouchingMovementSpeed;
-		} else {
+		} 
+		else 
+		{
 			currentCrouchingMovementSpeed = 1.0f;
 		}
 
 		//speed decrease upon aiming down the sights
-		if (Input.GetKey ("o")) {
+		if (Input.GetKey ("o")) 
+		{
 			currentAimDownSightMovementSpeed = aimDownSightMovementSpeed;
-		} else {
+		} 
+		else 
+		{
 			currentAimDownSightMovementSpeed = 1.0f;
 		}
 
-		//speed varies upon sprinting, crouching, and aiming down the sights. if the conditions of those three don't apply the speed multiply
-		//won't change current movement speed
+		//movement speed changes based on multiplier
 		verticalSpeed = Input.GetAxis ("Vertical") * movementSpeed * currentSprintSpeed * currentCrouchingMovementSpeed * 
 			currentAimDownSightMovementSpeed;
 		horizontalSpeed = Input.GetAxis ("Horizontal") * movementSpeed * currentSprintSpeed * currentCrouchingMovementSpeed * 
 			currentAimDownSightMovementSpeed;
-		/*print ("after");
-		print ("norm" + movementSpeed);
-		print ("fast" + sprintSpeed);
-		print ("ducking" + crouchingMovementSpeed);
-		print ("total" + movementSpeed * sprintSpeed / movementSpeed);*/
 
 		Vector3 speed = new Vector3 (horizontalSpeed, 0, verticalSpeed);//the player moves along the xz plane
 		
@@ -129,32 +125,32 @@ public class FirstPersonController : MonoBehaviour {
 
 	//player will move downward with a lower camera view
 	void playerCrouching(){
-		if (Input.GetButton ("Crouch")) {
-			if (playerController.height > playerCrouchHeight) {//keep crouching until minimum height
+		if (Input.GetButton ("Crouch")) 
+		{
+			//keep crouching until minimum height
+			if (playerController.height > playerCrouchHeight) 
+			{
 				playerController.height -= crouchingSpeedTransition;
-				//playerController.height = playerCrouchHeight;
 			}
 
-		} else if (playerController.height < playerHeight) {//keep standing until maximum height
+		}
+		//keep standing until maximum height
+		else if (playerController.height < playerHeight) 
+		{
 			playerController.height += crouchingSpeedTransition;
 		}
 		
-		playerCurrentHeight = playerController.height;//change game controller to match height
-		if (playerCurrentHeight >= playerHeight) {//boolean to determine whether sprinting allowed
+		playerCurrentHeight = playerController.height;//change game controller or player to match height
+
+		//isCrouching takes a value to decide whether to sprint or not
+		if (playerCurrentHeight >= playerHeight) 
+		{
 			isCrouching = false;
-		} else {
+		} 
+		else 
+		{
 			isCrouching = true;
 		}
 	}
 
-	/*void playerjump(){//player jumps into the air once
-		if (Input.GetKey ("j")) {//PLEASE CHANGE THIS INTO A BUTTON
-			print ("You press the jump button");
-			float jumpHeight = 2.0f;
-			float jumpSpeed = 9.8f;
-			Vector3 jumpPosition = new Vector3(horizontalSpeed, jumpHeight + transform.position.y, verticalSpeed);
-
-			Vector3.MoveTowards (transform.position, jumpPosition, jumpSpeed * Time.deltaTime); 
-		}
-	}*/
 }
